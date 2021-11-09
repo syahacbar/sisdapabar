2c2f3b6aa2950d486f9bde483fde168182698a49
ariesrutung <ariesrutung@gmail.com>
2021-11-10 04:33:44 +0900

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -25,10 +25,10 @@ date_default_timezone_set('Asia/Jayapura');
 |
 */
 // $config['base_url'] = '';
-// $config['base_url'] = 'https://localhost/sisdapabar/';
-$root = "http://" . $_SERVER['HTTP_HOST'];
-$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-$config['base_url']    = "$root";
+$config['base_url'] = 'https://localhost/sisdapabar/';
+// $root = "http://" . $_SERVER['HTTP_HOST'];
+// $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+// $config['base_url']    = "$root";
 
 /*
 |--------------------------------------------------------------------------
